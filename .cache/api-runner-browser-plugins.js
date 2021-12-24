6c3c46c2ae3c0a8393b653ca071124f785198ffd
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2021-12-24 16:45:37 -0500

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Gatsby Starter Leaflet","short_name":"Gatsby Starter Leaflet","start_url":"/","icon":"src/assets/images/react-leaflet-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"dda9d5637adfc81f012fb809eec58896"},
-    }]
